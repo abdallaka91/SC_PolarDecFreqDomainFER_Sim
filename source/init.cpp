d5571248efc83f4d706c8bc9579e1a463c7359f0
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-07-16 15:15:46 +0200

    Update init.cpp

--- source/init.cpp
+++ source/init.cpp
@@ -230,9 +230,20 @@ void PoAwN::init::LoadTables(PoAwN::structures::base_code_t &code,
                              const uint16_t *GF_polynom_primitive)
 {
     uint16_t prim_pol = GF_polynom_primitive[code.p - 2];
+    
+    std::cout << "(II) - PoAwN::GFtools::GF_bin_seq_gen" << std::endl;
     PoAwN::GFtools::GF_bin_seq_gen(code.q, prim_pol, table.BINGF, table.BINDEC);
-    PoAwN::GFtools::GF_bin2GF(table.BINGF, table.DECGF, table.GFDEC); // x^-inf=GFDEC[0]=0, x^0=GFDEC[1]=1, , x^1=GFDEC[2]=2,.., , x^(q-2)=GFDEC[q-1] and GFDEC[DECGF[i]]=i
+    
+    std::cout << "(II) - PoAwN::GFtools::GF_bin2GF" << std::endl;
+    PoAwN::GFtools::GF_bin2GF     (table.BINGF, table.DECGF, table.GFDEC); // x^-inf=GFDEC[0]=0, x^0=GFDEC[1]=1, , x^1=GFDEC[2]=2,.., , x^(q-2)=GFDEC[q-1] and GFDEC[DECGF[i]]=i
+    
+    std::cout << "(II) - PoAwN::GFtools::GF_add_mat_gen" << std::endl;
     PoAwN::GFtools::GF_add_mat_gen(table.BINGF, table.DECGF, table.GFDEC, table.ADDGF, table.ADDDEC);
+    
+    std::cout << "(II) - PoAwN::GFtools::GF_mul_mat_gen" << std::endl;
     PoAwN::GFtools::GF_mul_mat_gen(table.DECGF, table.MULGF, table.MULDEC);
+    
+    std::cout << "(II) - PoAwN::GFtools::GF_div_mat_gen" << std::endl;
     PoAwN::GFtools::GF_div_mat_gen(table.DECGF, table.DIVGF, table.DIVDEC);
+    std::cout << "(II) - End of PoAwN::GFtools section" << std::endl;
 }

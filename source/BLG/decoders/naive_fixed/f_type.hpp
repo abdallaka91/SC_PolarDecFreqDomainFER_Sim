628d7562c5d82dd07c02d61ee348b1cb6c6fa350
abdallaka91 <154563304+abdallaka91@users.noreply.github.com>
2025-11-05 10:59:28 +0100

    ap_fixed added and Bertrand added normalization

--- source/BLG/decoders/naive_fixed/f_type.hpp
+++ source/BLG/decoders/naive_fixed/f_type.hpp
@@ -7,12 +7,27 @@
 #include "include/ap_fixed.h"
 //
 #define NBITS   32
-#define NFRAC   22
+#define NFRAC   24
 //
 struct symbols_f {
     ap_fixed<NBITS, NFRAC> value[_GF_];
     bool is_freq;
 };
+//
+//
+//
+//
+//
+inline float is_issue(const ap_fixed<NBITS, NFRAC>* value)
+{
+    float maxv = 0.f;
+    for (size_t i = 0; i < _GF_; i++)
+    {
+        const float v = value[i].to_float();
+        maxv = std::fmaxf( maxv, std::fabs( v ) );
+    }
+    return maxv;
+}
 //
 //
 //
--- source/BLG/decoders/naive_fixed/g_function.hpp
+++ source/BLG/decoders/naive_fixed/g_function.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "f_argmax.hpp"
+#include "f_fwht_norm.hpp"
 #include "f_normalize.hpp"
+
+//#define _TEST_
 
 template <int gf_size>
 void g_function(
@@ -10,22 +13,81 @@ void g_function(
     symbols_f * __restrict src_b, // the lower value set from the right side of the graph
     uint32_t    src_c)    // the computed symbols coming from the left side of the graph
 {
-    if (src_a->is_freq == true) {
+#ifdef _TEST_
+    if ( is_issue(src_a->value) > 1.f ) {
+        printf("(EE) Error in %s %d\n", __FILE__, __LINE__);
+        printf("(EE) value = %f\n", is_issue(src_a->value));
+        exit(EXIT_FAILURE);
+    }
+#endif
+
+    if (src_a->is_freq == true)
+    {
+        //
+        // Pour eviter que la dynamique explose en interne dans la FWHT, il
+        // faut appliquer la normalisation que l'on n'a pas fait en sortie
+        // de la FWHT (fonction F)
+        //
+        const ap_fixed<NBITS, NFRAC> factor = norm_factor_lwht<gf_size>();
+        for (size_t i = 0; i < gf_size; i++) {
+            src_a->value[i] = src_a->value[i] * factor;
+        }
+        // traitement normal en suivant
         fwht_norm<gf_size>(src_a->value);
         src_a->is_freq = false;
     }
 
+#ifdef _TEST_
+    if ( is_issue(src_a->value) > 1.f ) {
+        printf("(EE) Error in %s %d\n", __FILE__, __LINE__);
+        printf("(EE) value = %f\n", is_issue(src_a->value));
+        exit(EXIT_FAILURE);
+    }
+#endif
+
     if (src_b->is_freq == true) {
+        //
+        // Pour eviter que la dynamique explose en interne dans la FWHT, il
+        // faut appliquer la normalisation que l'on n'a pas fait en sortie
+        // de la FWHT (fonction F)
+        //
+        const ap_fixed<NBITS, NFRAC> factor = norm_factor_lwht<gf_size>();
+        for (size_t i = 0; i < gf_size; i++) {
+            src_b->value[i] = src_b->value[i] * factor;
+        }
+        // traitement normal en suivant
         fwht_norm<gf_size>(src_b->value);
         src_b->is_freq = false;
     }
+
+#ifdef _TEST_
+    if ( is_issue(src_b->value) > 1.f ) {
+        printf("(EE) Error in %s %d\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+#endif
 
     for (size_t i = 0; i < gf_size; i++) {
         const int idx   = src_c ^ i;
         dst->value[idx] = src_a->value[i] * src_b->value[idx];
     }
 
+#ifdef _TEST_
+    if ( is_issue(dst->value) > 1.f ) {
+        printf("(EE) Error in %s %d\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+#endif
+
     f_normalize<gf_size>(dst->value); // temporal
+
+#ifdef _TEST_
+    if ( is_issue(dst->value) > 1.f ) {
+        printf("(EE) Error in %s %d\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+#endif
+
     dst->is_freq = false;
 }
 //

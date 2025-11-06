627e0c2d3a01c736027b184a654f806ec8887bf3
abdallaka91 <154563304+abdallaka91@users.noreply.github.com>
2025-11-06 11:45:33 +0100

    now VN output scaling is done simply by factor multiplicationgit add .

--- source/BLG/decoders/naive_fixed/f_normalize.hpp
+++ source/BLG/decoders/naive_fixed/f_normalize.hpp
@@ -1,6 +1,7 @@
 //
 // Created by legal on 03/07/2025.
-//
+
+#define _meth_ 2
 #pragma once
 //
 //
@@ -9,6 +10,8 @@
 //
 //
 //
+
+#if _meth_ == 0
 template <int gf_size>
 void f_normalize(ap_fixed<NBITS, NINTG> *tab)
 {
@@ -29,13 +32,52 @@ void f_normalize(ap_fixed<NBITS, NINTG> *tab)
         tab[i] /= sum;
     }
 
-    float sum2 =0.0f;
+    float sum2 = 0.0f;
     for (int i = 0; i < gf_size; i += 1)
     {
         sum2 += float(tab[i]);
     }
     // printf("%f, %f\n", sum1, sum2);
 }
+
+#else
+template <int gf_size>
+void f_normalize(ap_fixed<NBITS, NINTG> *tab)
+{
+    const int FRA_BITS = NBITS - NINTG;
+
+    ap_fixed<NBITS, NINTG> sum = 0.0f;
+    ap_fixed<NBITS, NINTG> one = 1.0f;
+    ap_fixed<NBITS, NINTG> zero = 0.0f;
+    for (int i = 0; i < gf_size; i++)
+        sum += tab[i];
+
+    if ((sum < one) && (sum != zero))
+    {
+        ap_ufixed<NBITS, 0> u_sum = sum;
+        ap_ufixed<FRA_BITS, 0> frac = u_sum;
+        ap_uint<FRA_BITS> raw = frac.range(FRA_BITS - 1, 0);
+        int lzc;
+        if (FRA_BITS <= 32)
+            lzc = __builtin_clz((uint32_t)raw) - (32 - FRA_BITS);
+        else
+            lzc = __builtin_clzll((uint64_t)raw) - (64 - FRA_BITS);
+        ap_fixed<NBITS, NINTG> sum_norm = sum << lzc;
+        ap_ufixed<NBITS, 0> t = sum_norm;
+        bool isPow2 = (t & (t - 1)) == 0;
+
+        ap_fixed<NBITS, NINTG> inv_norm;
+        if (isPow2)
+            inv_norm = one;
+        else
+            inv_norm = (one / sum_norm);
+
+        for (int i = 0; i < gf_size; i++)
+            tab[i] = (tab[i] * inv_norm) << lzc;
+    }
+}
+
+#endif
 //
 //
 //

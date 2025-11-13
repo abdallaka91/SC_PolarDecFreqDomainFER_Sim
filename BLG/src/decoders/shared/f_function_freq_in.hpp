a3dde82a0d9265dd79a6222e65d61e3b97fc1363
Abdallah abdallah <abdallah.91ka@gmail.com>
2025-11-13 19:21:42 +0100

    simulator_dec_fft_A.cpp test on N=64

---
+++ BLG/src/decoders/shared/f_function_freq_in.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "features/archi.hpp"
+
+template <uint32_t gf_size> inline __attribute__((always_inline)) void f_function_freq_in(
+    symbols_t * __restrict dst,
+    const symbols_t * __restrict src_a,
+    const symbols_t * __restrict src_b)
+{
+    //
+    // Element-wise multiplication of the two input symbols because we are in frequency domain !
+    //
+    for (size_t i = 0; i < int(gf_size); i++) {
+        dst->value[i] =  src_a->value[i] * src_b->value[i]; // TODO : attention au facteur 10x qui est magique !!!
+    }
+    dst->is_freq = true;
+//  normalize<gf_size>(dst->value);
+}
+//
+//
+//
+//
+//

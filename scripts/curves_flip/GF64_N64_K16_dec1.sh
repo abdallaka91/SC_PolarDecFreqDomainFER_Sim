cd25a5f8f536b6d9a250d8b88004a26ca06fe9f9
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2026-04-03 20:05:25 +0200

    scripts

---
+++ scripts/curves_flip/GF64_N64_K16_dec1.sh
@@ -0,0 +1,12 @@
+rm results/GF64_N64_K16_dec1.txt
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -16.00 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -15.75 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -15.50 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -15.25 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -15.00 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -14.75 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -14.50 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -14.25 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -14.00 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -13.75 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -cw 100000000 -snr -13.50 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40

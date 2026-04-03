2542056469ce87c1cdc752ca596b8a8df1fa70ae
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2026-04-03 19:51:08 +0200

    Update GF64_N64_K32_scf-oracle-1.sh

--- scripts/curves_flip/GF64_N64_K32_scf-oracle-1.sh
+++ scripts/curves_flip/GF64_N64_K32_scf-oracle-1.sh
@@ -1,12 +1,12 @@
 rm results/GF64_N64_K32_scf-oracle{1}.txt
-./build/Sim2 100000000 -10.0 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -9.75 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -9.50 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -9.25 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -9.00 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -8.75 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -8.50 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -8.25 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -8.00 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -7.75 64 64 32 scf-oracle{1} 100
-./build/Sim2 100000000 -7.50 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -10.0 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -9.75 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -9.50 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -9.25 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -9.00 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -8.75 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -8.50 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -8.25 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -8.00 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -7.75 64 64 32 scf-oracle{1} 100
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 100000000 -7.50 64 64 32 scf-oracle{1} 100

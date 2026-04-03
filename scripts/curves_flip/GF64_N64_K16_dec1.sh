f17f3f387310563c551f026c1a135e29a37eae8a
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2026-04-03 22:18:56 +0200

    Update GF64_N64_K16_dec1.sh

--- scripts/curves_flip/GF64_N64_K16_dec1.sh
+++ scripts/curves_flip/GF64_N64_K16_dec1.sh
@@ -24,3 +24,5 @@ LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -10.75 -q 64 -N 64 -K 16 -dec de
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -10.50 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -10.25 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -10.00 -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -9.75  -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -9.50  -q 64 -N 64 -K 16 -dec dec1 -errors 100 -cores 40

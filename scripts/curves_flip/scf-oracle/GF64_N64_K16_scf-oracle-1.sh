7eaf6f07e67b5f9e88be2dc04fa7ada9da24eceb
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2026-04-04 12:06:50 +0200

    Code and script refactoring

--- scripts/curves_flip/scf-oracle/GF64_N64_K16_scf-oracle-1.sh
+++ scripts/curves_flip/scf-oracle/GF64_N64_K16_scf-oracle-1.sh
@@ -1,4 +1,4 @@
-rm results/GF64_N64_K16_scf-oracle{1}.txt
+rm results/GF64_N64_K16_scl-oracle{1}.txt
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -16.00 -q 64 -N 64 -K 16 -dec scf-oracle{1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -15.75 -q 64 -N 64 -K 16 -dec scf-oracle{1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -15.50 -q 64 -N 64 -K 16 -dec scf-oracle{1} -errors 100 -cores 40

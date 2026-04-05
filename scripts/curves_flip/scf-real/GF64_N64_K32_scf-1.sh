54b3c736c25bc64e72dcc023fe153edd61d216c8
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2026-04-05 21:20:12 +0200

    Updates

--- scripts/curves_flip/scf-real/GF64_N64_K32_scf-1.sh
+++ scripts/curves_flip/scf-real/GF64_N64_K32_scf-1.sh
@@ -1,4 +1,4 @@
-rm results/GF64_N64_K32_scf-oracle{1}.txt
+rm results/GF64_N64_K32_scf{1}.txt
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -10.0 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -9.70 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -9.50 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
@@ -9,4 +9,5 @@ LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -8.50 -q 64 -N 64 -K 32 -dec scf
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -8.25 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -8.00 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.75 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
-#LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.50 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.50 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40
+LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.25 -q 64 -N 64 -K 32 -dec scf{1} -errors 100 -cores 40

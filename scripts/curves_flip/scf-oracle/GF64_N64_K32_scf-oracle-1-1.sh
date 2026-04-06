cb1857c8a5cea60a479b042cc8b1029a07bc10d1
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2026-04-06 17:54:09 +0200

    Update SNR range for faster evaluations

--- scripts/curves_flip/scf-oracle/GF64_N64_K32_scf-oracle-1-1.sh
+++ scripts/curves_flip/scf-oracle/GF64_N64_K32_scf-oracle-1-1.sh
@@ -18,4 +18,4 @@ LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -8.25  -q 64 -N 64 -K 32 -dec sc
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -8.00  -q 64 -N 64 -K 32 -dec scf-oracle{1-1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.75  -q 64 -N 64 -K 32 -dec scf-oracle{1-1} -errors 100 -cores 40
 LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.50  -q 64 -N 64 -K 32 -dec scf-oracle{1-1} -errors 100 -cores 40
-LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.25  -q 64 -N 64 -K 32 -dec scf-oracle{1-1} -errors 100 -cores 40
+#LD_PRELOAD=./libNbScFFTdec.so ./build/Sim2 -snr -7.25  -q 64 -N 64 -K 32 -dec scf-oracle{1-1} -errors 100 -cores 40

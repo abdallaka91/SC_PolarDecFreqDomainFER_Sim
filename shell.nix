1babe7e51fa759b13ba01b7b581e1cdfdc7cb60c
Abdallah Abdallah <abdallah.abdallah@univ-ubs.fr>
2026-09-01 22:01:04 +0200

    test1

--- shell.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs> {};
-mkShell {
-  NIX_ENFORCE_NO_NATIVE=0;
-
-  name = "env";
-  nativeBuildInputs = [ cmake clang gcc ccache ];
-  buildInputs = [ fftw fftwFloat llvmPackages.openmp ];
-}
